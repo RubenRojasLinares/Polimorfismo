f91d39f6229d5884292fdc2537fa892d539ab43c
Ruben Rojas <ruben.rojaslinares@ouutlook.com>
2020-02-11 15:53:50 -0600

    Clases Abstractas

---
+++ ClasesAbstractas/ClaseAbstractaEjemplo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesAbstractas
+{
+    abstract class ClaseAbstractaEjemplo
+    {
+        abstract public int Area();
+    }
+    class Cuadrado : ClaseAbstractaEjemplo {
+        int lado = 0;
+        public Cuadrado(int n) {
+            this.lado = n;
+        }
+
+        public override int Area()
+        {
+            return lado * lado;
+        }
+    }
+}
---
+++ ClasesAbstractas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado cuadrado = new Cuadrado(12);
+            Console.WriteLine("Area del cuadrado ={0}",cuadrado.Area());
+            Console.Read();
+        }
+    }
+}
